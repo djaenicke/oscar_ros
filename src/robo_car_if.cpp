fd8c4027ec77f15f3c3b22fbc858987505094fd6
Devin <djaenicke.92@gmail.com>
2020-05-14 22:06:30 -0400

    Robot is controllable like the TurtleBot

--- src/robo_car_if.cpp
+++ src/robo_car_if.cpp
@@ -1,54 +1,97 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "serial/serial.h"
+#include "robo_car_if/state.h"
+#include "robo_car_if/cmd.h"
+#include <signal.h>
+#include <termios.h>
+#include <stdio.h>
 
-using serial::Serial;
-
-// Serial stuff
-std::string port = "/dev/ttyACM0";
-Serial * serial_port;
+#define KEYCODE_R 0x43 
+#define KEYCODE_L 0x44
+#define KEYCODE_F 0x41
+#define KEYCODE_B 0x42
+#define KEYCODE_S 0x73
 
 int main(int argc, char **argv) {
+  char c;
+  bool dirty = false;
+  int kfd = 0;
+  struct termios cooked, raw;
+
+  robo_car_if::cmd cmd_msg;
 
   ros::init(argc, argv, "robo_car_if");
 
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
+  ros::Publisher cmd_pub = nh.advertise<robo_car_if::cmd>("robo_car_cmd", 100);
 
   ros::Rate loop_rate(1/0.025); // Loop rate in Hz
 
-  // Setup the serial port
-  serial_port = new Serial();
+  // get the console in raw mode
+  tcgetattr(kfd, &cooked);
+  memcpy(&raw, &cooked, sizeof(struct termios));
+  raw.c_lflag &= ~(ICANON | ECHO);
+  // Setting a new line, then end of file
+  raw.c_cc[VEOL] = 1;
+  raw.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &raw);
 
-  serial_port->setPort(port);
-  serial_port->setBaudrate(115200);
-  serial_port->setParity(serial::parity_none);
-  serial_port->setStopbits(serial::stopbits_one);
-  serial_port->setBytesize(serial::eightbits);
-  serial::Timeout to = serial::Timeout::simpleTimeout(10);
-  serial_port->setTimeout(to);
-
-  // Open the serial port
-  serial_port->open();
-
-  if (serial_port->isOpen()) {
-    ROS_INFO("Serial port open");
-  }
+  puts("Reading from keyboard");
+  puts("---------------------------");
+  puts("Use arrow keys to move the robot.");
 
   while (ros::ok()) {
-    std_msgs::String msg;
-    size_t bytes_avail;
+    // get the next event from the keyboard
+    if (read(kfd, &c, 1) < 0) {
+      perror("read():");
+      exit(-1);
+    }
+    switch (c) {
+      case KEYCODE_F:
+        puts("FORWARD");
+        cmd_msg.r_wheel_sp = 15.0;
+        cmd_msg.l_wheel_sp = 15.0;
+        cmd_msg.stop = 0;
+        dirty = true;
+        break;
+      case KEYCODE_B:
+        puts("BACK");
+        cmd_msg.r_wheel_sp = -15.0;
+        cmd_msg.l_wheel_sp = -15.0;
+        cmd_msg.stop = 0;
+        dirty = true;
+        break;
+      case KEYCODE_L:
+        puts("LEFT");
+        cmd_msg.r_wheel_sp = -6.0;
+        cmd_msg.l_wheel_sp =  6.0;
+        cmd_msg.stop = 0;
+        dirty = true;
+        break;
+      case KEYCODE_R:
+        puts("RIGHT");
+        cmd_msg.r_wheel_sp =  6.0;
+        cmd_msg.l_wheel_sp = -6.0;
+        cmd_msg.stop = 0;
+        dirty = true;
+        break;
+      case KEYCODE_S:
+        puts("STOP");
+        cmd_msg.r_wheel_sp = 0.0;
+        cmd_msg.l_wheel_sp = 0.0;
+        cmd_msg.stop = 1;
+        dirty = true;
+        break;
+    }
 
-    bytes_avail = serial_port->available();
-
-    if (bytes_avail) {
-      serial_port->read((uint8_t *) msg.data.c_str(), bytes_avail);
-      ROS_INFO("%s", msg.data.c_str());
+    if (dirty == true) {
+      cmd_pub.publish(cmd_msg);
+      dirty = false;
     }
 
     ros::spinOnce();
-
     loop_rate.sleep();
   }
 
   return 0;
 }
+ 
