b70d2e6b9effc63e11da81a8bfdcb64f57a4deff
Devin <djaenicke.92@gmail.com>
2020-12-20 22:39:50 -0700

    added madgwick filter for mpu6050

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -11,24 +11,6 @@
 #include <signal.h>
 #include <vector>
 
-#define XX 0
-#define XY 1
-#define XZ 2
-#define YX 3
-#define YY 4
-#define YZ 5
-#define ZX 6
-#define ZY 7
-#define ZZ 8
-
-#define MPU_AX_VARIANCE 0.000191916
-#define MPU_AY_VARIANCE 0.000207292
-
-#define MPU_GZ_VARIANCE 2.95646E-05
-
-#define FXOS_AX_VARIANCE 2.71782E-05
-#define FXOS_AY_VARIANCE 4.62584E-05
-
 static void StateMsgUpdateCallBack(const oscar_pi::state::ConstPtr& msg);
 
 // Pubs and Subs
@@ -36,14 +18,12 @@ static ros::Subscriber state_sub;
 static ros::Publisher odom_pub;
 static ros::Publisher footprint_pub;
 static ros::Publisher imu_mpu_pub;
-static ros::Publisher imu_fxos_pub;
 
 // ServiceClients
 static ros::ServiceClient reset_ekf_pose;
 
 static nav_msgs::Odometry odom;
 static sensor_msgs::Imu mpu;
-static sensor_msgs::Imu fxos;
 static geometry_msgs::PolygonStamped robot_polygon;
 
 static ros::Time current_time, last_time;
@@ -71,8 +51,7 @@ int main(int argc, char **argv)
 
   odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 100);
   footprint_pub = nh.advertise<geometry_msgs::PolygonStamped>("robot_footprint", 100);
-  imu_mpu_pub = nh.advertise<sensor_msgs::Imu>("imu_mpu", 100);
-  imu_fxos_pub = nh.advertise<sensor_msgs::Imu>("imu_fxos", 100);
+  imu_mpu_pub = nh.advertise<sensor_msgs::Imu>("imu/data_raw", 100);
 
   // Configure the robot's footprint for rviz
   int num_points;
@@ -91,13 +70,6 @@ int main(int argc, char **argv)
   robot_polygon = robot_footprint.GetPolyStampedMsg();
 
   // Set parameters
-  float embedded_update_rate;
-  if (!nh.getParam("/odometry/embeddded_update_rate", embedded_update_rate))
-  {
-    ROS_ERROR("embedded_update_rate rosparam undefined");
-    ros::shutdown();
-  }
-
   if (!nh.getParam("/odometry/wheel_base", wheel_base))
   {
     ROS_ERROR("wheel_base rosparam undefined");
@@ -110,26 +82,14 @@ int main(int argc, char **argv)
     ros::shutdown();
   }
 
-  // Init EKF messages
   mpu.header.frame_id = "base_link";
-  fxos.header.frame_id = "base_link";
 
   odom.header.frame_id = "odom";
   odom.child_frame_id = "base_link";
 
-  mpu.angular_velocity_covariance[ZZ] = MPU_GZ_VARIANCE;
-
-  mpu.linear_acceleration_covariance[XX] = MPU_AX_VARIANCE;
-  fxos.linear_acceleration_covariance[XX] = FXOS_AX_VARIANCE;
-
-  mpu.linear_acceleration_covariance[YY] = MPU_AY_VARIANCE;
-  fxos.linear_acceleration_covariance[YY] = FXOS_AY_VARIANCE;
-
-  // TODO need to revisit these
-  odom.twist.covariance[0] = 1e-3;   // x with respect to x
+  odom.twist.covariance[0] = 1e-5;   // x with respect to x
   odom.twist.covariance[7] = 1e-6;   // y with respect to y
-  odom.twist.covariance[35] = 1e-4;  // rotation about Z axis with respect to rotation about Z axis
-  odom.pose.covariance[35] = 1e-4;   // rotation about Z axis with respect to rotation about Z axis
+  odom.twist.covariance[35] = 1e-2;  // rotation about Z axis with respect to rotation about Z axis
 
   ros::spin();
   return 0;
@@ -140,8 +100,6 @@ static void StateMsgUpdateCallBack(const oscar_pi::state::ConstPtr& msg)
   static double zero_yaw;
   static bool init = false;
   double yaw, dt;
-
-  geometry_msgs::Quaternion fxos_quat;
 
   // Compute time between state messages
   current_time = ros::Time::now();
@@ -157,22 +115,11 @@ static void StateMsgUpdateCallBack(const oscar_pi::state::ConstPtr& msg)
 
   double delta_x = (x_dot * cos(th)) * dt;
   double delta_y = (x_dot * sin(th)) * dt;
-  double delta_th = th_dot * dt;
 
   x += delta_x;
   y += delta_y;
-  th += delta_th;
-
-  // Since all odometry is 6DOF we'll need a quaternion created from yaw
-  geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
   odom.header.stamp = current_time;
-
-  odom.pose.pose.position.x = x;
-  odom.pose.pose.position.y = y;
-  odom.pose.pose.position.z = 0.0;
-  odom.pose.pose.orientation = odom_quat;
-
   odom.twist.twist.linear.x = x_dot;
   odom.twist.twist.linear.y = 0;
   odom.twist.twist.angular.z = th_dot;
@@ -193,21 +140,4 @@ static void StateMsgUpdateCallBack(const oscar_pi::state::ConstPtr& msg)
   mpu.linear_acceleration.z = msg->mpu_az;
   imu_mpu_pub.publish(mpu);
   mpu.header.seq++;
-
-  fxos.header.stamp = current_time;
-  fxos.linear_acceleration.x = msg->fxos_ax;
-  fxos.linear_acceleration.y = msg->fxos_ay;
-  fxos.linear_acceleration.z = msg->fxos_az;
-
-  imu_fxos_pub.publish(fxos);
-  fxos.header.seq++;
-
-  if (!init)
-  {
-    zero_yaw = atan2(msg->fxos_mx, msg->fxos_my);
-    init = true;
-  }
-
-  yaw = atan2(msg->fxos_mx, msg->fxos_my) - zero_yaw;
-  fxos.orientation = tf::createQuaternionMsgFromYaw(yaw);
 }
